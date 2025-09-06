aeac814b71988f9b6b9ba19828ad02b667486f22
User2780 <User2780@DESKTOP-P6JENI4>
2025-09-06 09:32:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Полина";
+            //int age = 17;
+            //string city = "Волгоград";
+            //Console.WriteLine($"Имя - {name}, Возраст - {age}, Город - {city}");
+            //Console.WriteLine("Имя - {0}, Возраст - {1}, Город - {2}", name, age, city);
+            //byte bit1 = 1;
+            //byte bit2 = 2;
+            //sbyte bit3 = -125;
+            //short n1 = 23424;
+            //ushort n2 = 42424;
+            //int number = 464646464;
+            //uint number2 = 484848484;
+            //long number3 = 4448448488448;
+            //ulong number4 = 8884484848484;
+            //Int128 number5 = 94944944949494949;
+            //System.Int32 number6 = 3838383;
+
+            //bool alive = true;
+            //bool isdead = false;
+
+            //float floating1 = 3.14f;
+            //double floating2 = 3.14;
+            //decimal floating3 = 4.3030303m;
+
+            //string name = "Polina";
+            //char symbol = 'a';
+
+            //object ob1 = 3.3;
+            //Console.WriteLine(int.MaxValue);
+
+            //Console.WriteLine("Введите ваше имя: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя - {name}");
+
+            //Console.WriteLine("Введите ваше возраст: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Ваш возраст - {age}");
+
+            //var number = 10;
+
+            int number = 250;
+            byte number2 = (byte)(number+10);
+            Console.WriteLine(number2);
+        }
+    }
+}
